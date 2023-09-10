68e0ae548ea9287678ea2c01a910a93264e5af45
mcalisadeghi <ali.ke970@gmail.com>
2023-09-10 21:47:58 +0330

    add title

--- lib/screens/deatils/components/details_body.dart
+++ lib/screens/deatils/components/details_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants.dart';
 import 'package:flutter_application_1/models/product.dart';
 // import 'package:flutter_application_1/constants.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
@@ -27,8 +28,42 @@ class DetailsBody extends StatelessWidget {
                   ),
                   decoration: const BoxDecoration(
                     color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(
+                        25,
+                      ),
+                      topRight: Radius.circular(
+                        25,
+                      ),
+                    ),
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: kDefaulfPadding,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'arstocratic had bag',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        product.title,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineMedium
+                            ?.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
