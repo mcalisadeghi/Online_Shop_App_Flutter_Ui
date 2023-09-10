8dea2877cbc8b1969fe07a38119e08712afc7978
mcalisadeghi <ali.ke970@gmail.com>
2023-09-10 19:01:51 +0330

    add close icon

--- lib/screens/deatils/daatils.dart
+++ lib/screens/deatils/daatils.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/product.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 // import 'package:flutter_application_1/screens/components/categories.dart';
 // import 'package:flutter_application_1/constants.dart';
 
@@ -14,6 +15,16 @@ class DetailsSCreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: product.color,
         elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset(
+            'assets/icons/bck.svg',
+            colorFilter: const ColorFilter.mode(
+              Colors.white,
+              BlendMode.srcIn,
+            ),
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
       ),
     );
   }
