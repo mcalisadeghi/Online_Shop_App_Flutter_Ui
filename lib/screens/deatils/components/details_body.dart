1ff5545840d8efd2058dd9c3c784fcc4bd857e4c
mcalisadeghi <ali.ke970@gmail.com>
2023-09-10 22:36:31 +0330

    ColorDot

--- lib/screens/deatils/components/details_body.dart
+++ lib/screens/deatils/components/details_body.dart
@@ -22,26 +22,86 @@ class DetailsBody extends StatelessWidget {
             height: size.height,
             child: Stack(
               children: [
-                Container(
-                  height: 500,
-                  margin: EdgeInsets.only(
-                    top: size.height * 0.3,
-                  ),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(
-                        25,
-                      ),
-                      topRight: Radius.circular(
-                        25,
-                      ),
-                    ),
-                  ),
-                ),
+                ColorDot(size: size),
                 ProductTitleImage(product: product),
               ],
             ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ColorDot extends StatelessWidget {
+  const ColorDot({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // height: 500,
+      margin: EdgeInsets.only(
+        top: size.height * 0.3,
+      ),
+      padding: EdgeInsets.only(
+        top: size.height * 0.12,
+        left: kDefaulfPadding,
+        right: kDefaulfPadding,
+      ),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(
+            25,
+          ),
+          topRight: Radius.circular(
+            25,
+          ),
+        ),
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Column(
+                children: [
+                  const Text(
+                    'Color',
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(
+                      top: kDefaulfPadding / 4,
+                      right: kDefaulfPadding / 2,
+                    ),
+                    padding: const EdgeInsets.all(
+                      2.5,
+                    ),
+                    height: 24,
+                    width: 24,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: const Color(
+                          0xFF356C95,
+                        ),
+                      ),
+                    ),
+                    child: const DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Color(
+                          0xFF356C95,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
