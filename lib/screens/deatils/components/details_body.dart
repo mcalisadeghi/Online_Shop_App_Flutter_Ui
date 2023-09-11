8a3e81f1175c4b56dbe49c6894240bb3c037dec8
mcalisadeghi <ali.ke970@gmail.com>
2023-09-11 14:49:49 +0330

    build color and size calss

--- lib/screens/deatils/components/details_body.dart
+++ lib/screens/deatils/components/details_body.dart
@@ -43,39 +43,9 @@ class DetailsBody extends StatelessWidget {
                       ),
                     ),
                   ),
-                  child: const Column(
+                  child: Column(
                     children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Column(
-                            children: [
-                              Text(
-                                'Color',
-                              ),
-                              Row(
-                                children: [
-                                  ColorDot(
-                                    color: Color(
-                                      0xFF356C95,
-                                    ),
-                                  ),
-                                  ColorDot(
-                                    color: Color(
-                                      0xFF356C95,
-                                    ),
-                                  ),
-                                  ColorDot(
-                                    color: Color(
-                                      0xFF356C95,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                      ColorAndSize(product: product),
                     ],
                   ),
                 ),
@@ -85,6 +55,73 @@ class DetailsBody extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class ColorAndSize extends StatelessWidget {
+  const ColorAndSize({
+    super.key,
+    required this.product,
+  });
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Color',
+              ),
+              Row(
+                children: [
+                  ColorDot(
+                    color: Color(
+                      0xFF356C95,
+                    ),
+                    isSelected: true,
+                  ),
+                  ColorDot(
+                    color: Color(
+                      0xFFF8C078,
+                    ),
+                  ),
+                  ColorDot(
+                    color: Color(
+                      0xFFA29B9B,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: RichText(
+            text: TextSpan(
+              style: TextStyle(
+                color: kTextColor,
+              ),
+              children: [
+                TextSpan(
+                  text: 'Size\n',
+                ),
+                TextSpan(
+                  text: '${product.size}cm',
+                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
@@ -108,15 +145,18 @@ class ColorDot extends StatelessWidget {
       padding: const EdgeInsets.all(
         2.5,
       ),
-      height: 24,
-      width: 24,
+      height: 25,
+      width: 25,
       decoration: BoxDecoration(
         shape: BoxShape.circle,
-        border: Border.all(color: color),
+        border: Border.all(
+          color: isSelected ? color : Colors.transparent,
+        ),
       ),
       child: DecoratedBox(
         decoration: BoxDecoration(
           color: color,
+          shape: BoxShape.circle,
         ),
       ),
     );
